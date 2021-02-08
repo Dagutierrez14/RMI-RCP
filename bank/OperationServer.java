ae0caa8d1134082d4cf5b26f2e63ce874c6fe8ce
RafaelMendezUCAB <rrmendez.17@est.ucab.edu.ve>
2021-02-07 21:02:34 -0400

    created users.txt structure

--- bank/OperationServer.java
+++ bank/OperationServer.java
@@ -2,6 +2,10 @@ import java.rmi.*;
 import java.rmi.registry.*;
 import java.rmi.server.UnicastRemoteObject;
 
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 public class OperationServer {
 	public static void main(String args[]) {
 		try {
@@ -16,6 +20,14 @@ public class OperationServer {
 			registry.rebind("BANK", stub);
 
 			System.err.println("Server ready");
+
+			File myObj = new File("users.txt");
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				System.out.println(data);
+			}
+			myReader.close();
 
 		} catch (Exception e) {
 			System.out.println(e);
